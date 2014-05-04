f8deea5ee2207fa349d821bd19fe3fc6a4bc757b
Daniel Fein <danielfein@Daniels-MacBook-Pro.local>
2014-05-04 15:25:45 -0400

    dfein

--- Tethr/Tethr/RootViewController.m
+++ Tethr/Tethr/RootViewController.m
@@ -18,24 +18,24 @@
 //    }
 }
 
-- (BOOL)openSessionWithAllowLoginUI:(BOOL)allowLoginUI
-{
-    NSArray *permissions = @[@"basic_info", @"user_birthday"];
-    
-    return [FBSession openActiveSessionWithReadPermissions:permissions
-                                              allowLoginUI:allowLoginUI
-                                         completionHandler:^(FBSession *session, FBSessionState state, NSError *error) {
-     if (error) {
-         NSLog (@"Handle error %@", error.localizedDescription);
-         UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"LogIn failed" message:error.localizedDescription delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil];
-         [alertView show];
-         // terminate app
-     } else {
-         UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"LogIn success" message:@"You have been logged In successfully" delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil];
-         [alertView show];
-     }
- }];
-}
+//- (BOOL)openSessionWithAllowLoginUI:(BOOL)allowLoginUI
+//{
+//    NSArray *permissions = @[@"basic_info", @"user_birthday"];
+//    
+//    return [FBSession openActiveSessionWithReadPermissions:permissions
+//                                              allowLoginUI:allowLoginUI
+//                                         completionHandler:^(FBSession *session, FBSessionState state, NSError *error) {
+//     if (error) {
+//         NSLog (@"Handle error %@", error.localizedDescription);
+//         UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"LogIn failed" message:error.localizedDescription delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil];
+//         [alertView show];
+//         // terminate app
+//     } else {
+//         UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"LogIn success" message:@"You have been logged In successfully" delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil];
+//         [alertView show];
+//     }
+// }];
+//}
 
 - (void)facebookSessionStateChanged:(FBSession *)session state:(FBSessionState)state error:(NSError *)error
 {
